056a23cafd21915ad0230a1883e00351405cddc2
Isaac 'Atridas' Serrano Guasch <atridas87@gmail.com>
2023-11-23 13:46:23 +0000

    Adding a Sphere

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -3,24 +3,25 @@ mod floatops;
 mod rays;
 mod vectors;
 
-use std::ops::{Add, Div, Mul, Sub};
-
 pub use colors::*;
-use floatops::{Algebraic, LossyCast};
 pub use rays::*;
 pub use vectors::*;
 
-pub fn ray_color<T, U>(ray: &Ray<T>) -> RGB<U>
-where
-    T: Algebraic,
-    U: Copy,
-    f32: LossyCast<U>,
-    T: Div,
-    U: Add<Output = U> + Sub<Output = U> + Div<Output = U> + Mul<Output = U>,
-    T: LossyCast<U>,
-{
+pub fn ray_color(ray: &Ray<f64>) -> RGB<f32> {
+    if hit_sphere(Point3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return RGB::new(1.0, 0.0, 0.0);
+    }
+
     let unit_direction = Vector3::unit_vector(ray.direction());
-    let a = ((unit_direction.y + T::one()) / T::two()).lossy_cast();
-    RGB::new(1.0.lossy_cast(), 1.0.lossy_cast(), 1.0.lossy_cast()) * (1.0.lossy_cast() - a)
-        + RGB::new(0.5.lossy_cast(), 0.7.lossy_cast(), 1.0.lossy_cast()) * a
+    let a = ((unit_direction.y + 1.0) * 0.5) as f32;
+    RGB::new(1.0, 1.0, 1.0) * (1.0 - a) + RGB::new(0.5, 0.7, 1.0) * a
+}
+
+fn hit_sphere(center: Point3<f64>, radius: f64, r: &Ray<f64>) -> bool {
+    let oc = r.origin() - center;
+    let a = r.direction().dot(r.direction());
+    let b = oc.dot(r.direction()) * 2.0;
+    let c = oc.dot(oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+    discriminant >= 0.0
 }
