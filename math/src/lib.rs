5f71bc8efccf6a714335aa3781065aa8c48a0518
Isaac 'Atridas' Serrano Guasch <atridas87@gmail.com>
2023-11-23 16:13:41 +0000

    refactor code from main to library

--- math/src/lib.rs
+++ math/src/lib.rs
@@ -12,21 +12,3 @@ pub use hittable::*;
 pub use interval::*;
 pub use rays::*;
 pub use vectors::*;
-
-pub fn ray_color<T: Hittable<f64>>(ray: &Ray<f64>, world: &T) -> RGB<f32> {
-    match world.hit(ray, Interval::new(0.0, f64::INFINITY)) {
-        Some(record) => {
-            let normal_colour = RGB::new(
-                record.normal.x as f32,
-                record.normal.y as f32,
-                record.normal.z as f32,
-            );
-            (normal_colour + RGB::new(1.0, 1.0, 1.0)) * 0.5
-        }
-        None => {
-            let unit_direction = Vector3::unit_vector(ray.direction());
-            let a = ((unit_direction.y + 1.0) * 0.5) as f32;
-            RGB::new(1.0, 1.0, 1.0) * (1.0 - a) + RGB::new(0.5, 0.7, 1.0) * a
-        }
-    }
-}
--- one-weekend/src/main.rs
+++ one-weekend/src/main.rs
@@ -1,20 +1,8 @@
 use math::*;
+use one_weekend::Camera;
 use stb;
 
 fn main() {
-    // Image
-
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400usize;
-
-    // Calculate the image height, and ensure that it's at least 1.
-    let image_height = (image_width as f64 / aspect_ratio) as usize;
-    let image_height = if image_height < 1usize {
-        1usize
-    } else {
-        image_height
-    };
-
     // World
     let mut world: Vec<GeometricPrimitive<f64>> = Vec::new();
     world.push(GeometricPrimitive::Sphere(Sphere::new(
@@ -38,48 +26,18 @@ fn main() {
 
     // Camera
 
-    let focal_length = 1.0;
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * (image_width as f64 / image_height as f64);
-    let camera_center = Point3::new(0.0, 0.0, 0.0);
-
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vector3::new(0.0, -viewport_height, 0.0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    let pixel_delta_u = viewport_u / image_width as f64;
-    let pixel_delta_v = viewport_v / image_height as f64;
-
-    // Calculate the location of the upper left pixel.
-    let viewport_upper_left =
-        camera_center - Vector3::new(0.0, 0.0, focal_length) - viewport_u * 0.5 - viewport_v * 0.5;
-    let pixel00_loc = viewport_upper_left + (pixel_delta_u + pixel_delta_v) * 0.5;
+    let camera = Camera::new(16.0 / 9.0, 400);
 
     // Render
 
-    let mut data = Vec::with_capacity(image_width * image_height * 3);
-
-    for j in 0..image_height {
-        eprint!("\rScanlines remaining {} ", image_height - j);
-        for i in 0..image_width {
-            let pixel_center =
-                pixel00_loc + (pixel_delta_u * i as f64) + (pixel_delta_v * j as f64);
-            let ray_direction = pixel_center - camera_center;
-            let r = Ray::new(camera_center, ray_direction);
-
-            let rgb: RGB<f32> = ray_color(&r, &world);
-
-            data.push(rgb);
-        }
-    }
+    let data = camera.render(&world);
 
     eprint!("\rWriting image            ");
 
     stb::write_png(
         "image.png",
-        image_width as u32,
-        image_height as u32,
+        camera.get_image_width(),
+        camera.get_image_height(),
         3,
         &RGB::to_byte_array(&data),
     )
